3f59d4f9984263c8a77466a27cac6c9e9f8fce8c
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2019-07-15 09:40:49 -0400

    Updates

---
+++ precache-manifest.6693ec8e7762468cc629b8c0a6003f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29809dbb196234a9f3239fcdc1b4a2b",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "46e2692807b07a847ea3",
+    "url": "/Youtube-Clone/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/Youtube-Clone/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "46e2692807b07a847ea3",
+    "url": "/Youtube-Clone/static/js/main.bd1c3057.chunk.js"
+  },
+  {
+    "revision": "44189c6fc75b9f9f2a37",
+    "url": "/Youtube-Clone/static/js/runtime~main.cd741f44.js"
+  }
+]);
